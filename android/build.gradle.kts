03f3aee205276d450ef937f7165ac8e33b3e3500
Joël Gardet <jgardet@gmail.com>
2026-08-28 18:03:28 +0200

    Bump kotlinx-coroutines-android to 1.11.0 for parity with dsh-android.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -28,7 +28,7 @@ android {
 
 dependencies {
     api(project(":kotlin"))
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.11.0")
     implementation("androidx.annotation:annotation:1.9.1")
     androidTestImplementation("androidx.test:runner:1.6.2")
     androidTestImplementation("androidx.test:core:1.6.1")
